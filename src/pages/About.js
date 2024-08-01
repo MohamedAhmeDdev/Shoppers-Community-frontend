1c755c07e9d2dad9663ecd474fdfb00d23ef8382
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2024-08-01 13:50:51 +0300

    Changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -19,14 +19,14 @@ function About() {
             <h1 className="text-1xl font-bold mb-4">Your number 1 comparison destination for specific products online</h1>
             <p className="text-gray-600">Since 2024, Shophorizon's mission is to provide seekers with the insights they need to find the best products online based on their industry, size, budget and specific requirements. Every year, we connect millions of people with products and the vendors that meet their needs.</p>
           </div>
-          <img src="https://cdn.pixabay.com/photo/2024/03/31/00/49/shopping-8665807_1280.jpg" alt="Comparison Illustration" className="md:w-1/3 mt-6 md:mt-0 md:mr-6" />
+          <img src="https://cdn.pixabay.com/photo/2019/04/26/07/14/store-4156934_1280.png" alt="Comparison Illustration" className="md:w-1/3 mt-6 md:mt-0 md:mr-6" />
         </section>
         <section className="flex flex-col md:flex-row items-center mb-12">
           <div className="md:w-2/3">
             <h1 className="text-1xl font-bold mb-4">Your number 1 comparison destination for specific products online</h1>
             <p className="text-gray-600">Since 2024, Shophorizon's mission is to provide seekers with the insights they need to find the best products online based on their industry, size, budget and specific requirements. Every year, we connect millions of people with products and the vendors that meet their needs.</p>
           </div>
-          <img src="https://cdn.pixabay.com/photo/2024/03/31/00/49/shopping-8665807_1280.jpg" alt="Comparison Illustration" className="md:w-1/3 mt-6 md:mt-0 md:ml-6" />
+          <img src="https://cdn.pixabay.com/photo/2018/01/14/23/05/ecommerce-3082813_1280.jpg" alt="Comparison Illustration" className="md:w-1/3 mt-6 md:mt-0 md:ml-6" />
         </section>
       </main>
 
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,24 +6,28 @@ function Contact() {
 
 
       {/* Main Content */}
-      <main className="container mx-auto py-16 px-4">
-        <section className="text-center mb-12">
-          <h1 className="text-2xl font-bold mb-4">Contact Us</h1>
-          <img src="https://cdn.pixabay.com/photo/2016/11/22/19/17/buildings-1850129_1280.jpg" alt="Cityscape" className="mx-auto mb-6" />
+      <main className="w-full py-16 px-4">
+      <section className="text-center mb-12 relative">
+          <img
+            src="https://cdn.pixabay.com/photo/2016/11/22/19/17/buildings-1850129_1280.jpg"
+            alt="Cityscape"
+            className="w-full mb-6"
+          />
+          <h1 className="absolute bottom-3/4 inset-0 flex items-center justify-center text-3xl font-bold text-black">Contact Us</h1>
         </section>
         <section className="grid grid-cols-1 md:grid-cols-3 text-center mb-12">
-          <div className="mb-6 md:mb-0">
-            <i className="fas fa-map-marker-alt fa-3x mb-4 text-black"></i>
+        <div className="mb-6 md:mb-0 shadow-lg p-4 rounded">
+            <img src="https://cdn-icons-png.flaticon.com/128/535/535188.png" alt="Location Icon" className="mx-auto mb-4 h-12 w-12" />
             <h2 className="text-xl font-bold mb-2">Location</h2>
             <p className="text-gray-600">ShopHorizon<br />Street Lane<br />Avenue 11</p>
           </div>
-          <div className="mb-6 md:mb-0">
-            <i className="fas fa-phone-alt fa-3x mb-4 text-black"></i>
+          <div className="mb-6 md:mb-0 shadow-lg p-4 rounded">
+            <img src="https://cdn-icons-png.flaticon.com/128/159/159052.png" alt="Location Icon" className="mx-auto mb-4 h-12 w-12" />
             <h2 className="text-xl font-bold mb-2">Telephone</h2>
             <p className="text-gray-600">+2547012345678</p>
           </div>
-          <div>
-            <i className="fas fa-envelope fa-3x mb-4 text-black"></i>
+          <div className="mb-6 md:mb-0 shadow-lg p-4 rounded">
+            <img src="https://cdn-icons-png.flaticon.com/128/159/159052.png" alt="Location Icon" className="mx-auto mb-4 h-12 w-12" />
             <h2 className="text-xl font-bold mb-2">Mail</h2>
             <p className="text-gray-600">shophorizon@gmail.com</p>
           </div>
