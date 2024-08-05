f466cfed1bea043bab16e159bf2deda41f4c661f
ednakiruai <edna.kiruai@student.moringaschool.com>
2024-08-05 19:13:57 +0300

    Create API to fetch data

--- src/pages/ProductFilter.js
+++ src/pages/ProductFilter.js
@@ -2,7 +2,161 @@ import React from 'react'
 
 function ProductFilter() {
   return (
-    <div>ProductFilter</div>
+    <section class="py-24 relative">
+    <div class="w-full max-w-7xl mx-auto px-4 md:px-8">
+        <div class="flex flex-col lg:flex-row lg:items-center max-lg:gap-4 justify-between w-full">
+            <h1><span className='font-bold text-2xl'>Category Type </span>: Electronics</h1>
+        </div>
+        <svg class="my-7 w-full" xmlns="http://www.w3.org/2000/svg" width="1216" height="2" viewBox="0 0 1216 2"
+            fill="none">
+            <path d="M0 1H1216" stroke="#E5E7EB" />
+        </svg>
+        <div class="grid grid-cols-12">
+            <div class="col-span-12 md:col-span-3 w-full max-md:max-w-md max-md:mx-auto">
+              
+                <div class="mt-7 box rounded-xl border border-gray-300 bg-white p-6 w-full md:max-w-sm">
+                    <div class="flex items-center justify-between w-full pb-3 border-b border-gray-200 mb-7">
+                        <p class="font-medium text-base leading-7 text-black ">Filter</p>
+                        <p
+                            class="font-medium text-xs text-gray-500 cursor-pointer transition-all duration-500 hover:text-indigo-600">
+                            RESET</p>
+                    </div>
+
+                    <p class="font-medium text-sm leading-6 text-black mb-3">Categories</p>
+                    <div class="box flex flex-col gap-2 pb-10">
+                        <div class="flex items-center">
+                            <input id="checkbox-default-1" type="checkbox" value="" class="w-5 h-5 appearance-none border border-gray-300  rounded-md mr-2 hover:border-indigo-500 hover:bg-indigo-100 checked:bg-no-repeat checked:bg-center checked:border-indigo-500 checked:bg-indigo-100 checked:bg-[url('https://pagedone.io/asset/uploads/1689406942.svg')]"/>
+                            <label for="checkbox-default-1" class="text-xs font-normal text-gray-600 leading-4 cursor-pointer">Electronics</label>
+                        </div>
+                        <div class="flex items-center">
+                            <input id="checkbox-default-2" type="checkbox" value="" class="w-5 h-5 appearance-none border border-gray-300  rounded-md mr-2 hover:border-indigo-500 hover:bg-indigo-100 checked:bg-no-repeat checked:bg-center checked:border-indigo-500 checked:bg-indigo-100 checked:bg-[url('https://pagedone.io/asset/uploads/1689406942.svg')]"/>
+                            <label for="checkbox-default-2" class="text-xs font-normal text-gray-600 leading-4 cursor-pointer">Sports & Outdoors</label>
+                        </div>
+                        <div class="flex items-center">
+                            <input id="checkbox-default-3" type="checkbox" value="" class="w-5 h-5 appearance-none border border-gray-300  rounded-md mr-2 hover:border-indigo-500 hover:bg-indigo-100 checked:bg-no-repeat checked:bg-center checked:border-indigo-500 checked:bg-indigo-100 checked:bg-[url('https://pagedone.io/asset/uploads/1689406942.svg')]"/>
+                            <label for="checkbox-default-3" class="text-xs font-normal text-gray-600 leading-4 cursor-pointer">Clothing</label>
+                        </div>
+                        <div class="flex items-center">
+                            <input id="checkbox-default-3" type="checkbox" value="" class="w-5 h-5 appearance-none border border-gray-300  rounded-md mr-2 hover:border-indigo-500 hover:bg-indigo-100 checked:bg-no-repeat checked:bg-center checked:border-indigo-500 checked:bg-indigo-100 checked:bg-[url('https://pagedone.io/asset/uploads/1689406942.svg')]"/>
+                            <label for="checkbox-default-3" class="text-xs font-normal text-gray-600 leading-4 cursor-pointer">Furniture</label>
+                        </div>
+                        <div class="flex items-center">
+                            <input id="checkbox-default-3" type="checkbox" value="" class="w-5 h-5 appearance-none border border-gray-300  rounded-md mr-2 hover:border-indigo-500 hover:bg-indigo-100 checked:bg-no-repeat checked:bg-center checked:border-indigo-500 checked:bg-indigo-100 checked:bg-[url('https://pagedone.io/asset/uploads/1689406942.svg')]"/>
+                            <label for="checkbox-default-3" class="text-xs font-normal text-gray-600 leading-4 cursor-pointer">Others</label>
+                        </div>
+                    </div>
+
+                    <p class="font-medium text-sm leading-6 text-black mb-3">Products</p>
+                    <div class="box flex flex-col gap-2 pb-10">
+                        <div class="flex items-center">
+                            <input id="checkbox-default-1" type="checkbox" value="" class="w-5 h-5 appearance-none border border-gray-300  rounded-md mr-2 hover:border-indigo-500 hover:bg-indigo-100 checked:bg-no-repeat checked:bg-center checked:border-indigo-500 checked:bg-indigo-100 checked:bg-[url('https://pagedone.io/asset/uploads/1689406942.svg')]"/>
+                            <label for="checkbox-default-1" class="text-xs font-normal text-gray-600 leading-4 cursor-pointer">Phones</label>
+                        </div>
+                        <div class="flex items-center">
+                            <input id="checkbox-default-2" type="checkbox" value="" class="w-5 h-5 appearance-none border border-gray-300  rounded-md mr-2 hover:border-indigo-500 hover:bg-indigo-100 checked:bg-no-repeat checked:bg-center checked:border-indigo-500 checked:bg-indigo-100 checked:bg-[url('https://pagedone.io/asset/uploads/1689406942.svg')]"/>
+                            <label for="checkbox-default-2" class="text-xs font-normal text-gray-600 leading-4 cursor-pointer">Computers</label>
+                        </div>
+                        <div class="flex items-center">
+                            <input id="checkbox-default-3" type="checkbox" value="" class="w-5 h-5 appearance-none border border-gray-300  rounded-md mr-2 hover:border-indigo-500 hover:bg-indigo-100 checked:bg-no-repeat checked:bg-center checked:border-indigo-500 checked:bg-indigo-100 checked:bg-[url('https://pagedone.io/asset/uploads/1689406942.svg')]"/>
+                            <label for="checkbox-default-3" class="text-xs font-normal text-gray-600 leading-4 cursor-pointer">Camera</label>
+                        </div>
+                        <div class="flex items-center">
+                            <input id="checkbox-default-3" type="checkbox" value="" class="w-5 h-5 appearance-none border border-gray-300  rounded-md mr-2 hover:border-indigo-500 hover:bg-indigo-100 checked:bg-no-repeat checked:bg-center checked:border-indigo-500 checked:bg-indigo-100 checked:bg-[url('https://pagedone.io/asset/uploads/1689406942.svg')]"/>
+                            <label for="checkbox-default-3" class="text-xs font-normal text-gray-600 leading-4 cursor-pointer">Watches</label>
+                        </div>
+                    </div>
+
+
+                    <p class="font-medium text-sm leading-6 text-black mb-3">Ratings</p>
+                    <div class="box flex flex-col gap-2 pb-10">
+                        <div class="flex items-center">
+                            <input id="checkbox-default-1" type="checkbox" value="" class="w-5 h-5 appearance-none border border-gray-300  rounded-md mr-2 hover:border-indigo-500 hover:bg-indigo-100 checked:bg-no-repeat checked:bg-center checked:border-indigo-500 checked:bg-indigo-100 checked:bg-[url('https://pagedone.io/asset/uploads/1689406942.svg')]"/>
+                            <label for="checkbox-default-1" class="text-xs font-normal text-gray-600 leading-4 cursor-pointer">1</label>
+                        </div>
+                        <div class="flex items-center">
+                            <input id="checkbox-default-2" type="checkbox" value="" class="w-5 h-5 appearance-none border border-gray-300  rounded-md mr-2 hover:border-indigo-500 hover:bg-indigo-100 checked:bg-no-repeat checked:bg-center checked:border-indigo-500 checked:bg-indigo-100 checked:bg-[url('https://pagedone.io/asset/uploads/1689406942.svg')]"/>
+                            <label for="checkbox-default-2" class="text-xs font-normal text-gray-600 leading-4 cursor-pointer">2</label>
+                        </div>
+                        <div class="flex items-center">
+                            <input id="checkbox-default-3" type="checkbox" value="" class="w-5 h-5 appearance-none border border-gray-300  rounded-md mr-2 hover:border-indigo-500 hover:bg-indigo-100 checked:bg-no-repeat checked:bg-center checked:border-indigo-500 checked:bg-indigo-100 checked:bg-[url('https://pagedone.io/asset/uploads/1689406942.svg')]"/>
+                            <label for="checkbox-default-3" class="text-xs font-normal text-gray-600 leading-4 cursor-pointer">3</label>
+                        </div>
+                        <div class="flex items-center">
+                            <input id="checkbox-default-3" type="checkbox" value="" class="w-5 h-5 appearance-none border border-gray-300  rounded-md mr-2 hover:border-indigo-500 hover:bg-indigo-100 checked:bg-no-repeat checked:bg-center checked:border-indigo-500 checked:bg-indigo-100 checked:bg-[url('https://pagedone.io/asset/uploads/1689406942.svg')]"/>
+                            <label for="checkbox-default-3" class="text-xs font-normal text-gray-600 leading-4 cursor-pointer">4</label>
+                        </div>
+                    </div>
+
+                    <p class="font-medium text-sm leading-6 text-black mb-3">Payment Methods</p>
+                    <div class="box flex flex-col gap-2 pb-10">
+                        <div class="flex items-center">
+                            <input id="checkbox-default-1" type="checkbox" value="" class="w-5 h-5 appearance-none border border-gray-300  rounded-md mr-2 hover:border-indigo-500 hover:bg-indigo-100 checked:bg-no-repeat checked:bg-center checked:border-indigo-500 checked:bg-indigo-100 checked:bg-[url('https://pagedone.io/asset/uploads/1689406942.svg')]"/>
+                            <label for="checkbox-default-1" class="text-xs font-normal text-gray-600 leading-4 cursor-pointer">After delivery</label>
+                        </div>
+                        <div class="flex items-center">
+                            <input id="checkbox-default-2" type="checkbox" value="" class="w-5 h-5 appearance-none border border-gray-300  rounded-md mr-2 hover:border-indigo-500 hover:bg-indigo-100 checked:bg-no-repeat checked:bg-center checked:border-indigo-500 checked:bg-indigo-100 checked:bg-[url('https://pagedone.io/asset/uploads/1689406942.svg')]"/>
+                            <label for="checkbox-default-2" class="text-xs font-normal text-gray-600 leading-4 cursor-pointer">Before delivery</label>
+                        </div>
+                    </div>
+
+
+                    <button
+                        class="w-full py-2.5 mt-3 flex items-center justify-center gap-2 rounded-full bg-[#f7444e] text-white font-semibold text-xs shadow-sm shadow-transparent transition-all duration-500 hover:[#f7444e] hover:shadow-indigo-200  ">
+                        <svg width="17" height="16" viewBox="0 0 17 16" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M14.4987 13.9997L13.1654 12.6663M13.832 7.33301C13.832 10.6467 11.1457 13.333 7.83203 13.333C4.51832 13.333 1.83203 10.6467 1.83203 7.33301C1.83203 4.0193 4.51832 1.33301 7.83203 1.33301C11.1457 1.33301 13.832 4.0193 13.832 7.33301Z"
+                                stroke="white" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                        Search
+                    </button>
+
+                </div>
+            </div>
+            <div class="col-span-12 md:col-span-9">
+            <div class="relative m-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
+            <a class="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl" href="#">
+              <img class="object-cover" src="https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c25lYWtlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="product image" />
+            
+            </a>
+            <div class="mt-4 px-5 pb-5">
+              <a href="#">
+                <h5 class="text-md tracking-tight text-slate-900">Nike Air MX Super 2500 - Red</h5>
+              </a>
+              <div class="mt-2 mb-5 flex items-center justify-between">
+                <p>
+              
+                  <span class="text-md text-slate-900">$699</span>
+                </p>
+                <div class="flex items-center">
+                  <svg aria-hidden="true" class="h-5 w-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                  </svg>
+                  <svg aria-hidden="true" class="h-5 w-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                  </svg>
+                  <svg aria-hidden="true" class="h-5 w-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                  </svg>
+                  <svg aria-hidden="true" class="h-5 w-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                  </svg>
+                  <svg aria-hidden="true" class="h-5 w-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                  </svg>
+                  <span class="mr-2 ml-3 rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold">5.0</span>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+            </div>
+        </div>
+
+    </div>
+   </section>
+                                        
   )
 }
 
