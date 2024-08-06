3c016282340aa262ab82b616153682628414e273
Mohamed Ahmed <ma07041705@gmail.com>
2024-08-06 21:53:29 +0300

    update

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,9 @@
 module.exports = {
-    // Transform files using babel-jest
-    transform: {
-      '^.+\\.jsx?$': 'babel-jest', // This will handle .js and .jsx files
-    },
-    // Ignore transformations for specific modules
     transformIgnorePatterns: [
-      '/node_modules/(?!(@testing-library|some-other-package)/)', // Example for specific packages
+      "/node_modules/(?!(@testing-library)/)"
     ],
-    // Test environment
-    testEnvironment: 'jsdom', // Suitable for testing front-end code
-    // Other configurations
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // Path to setup files if needed
+    transform: {
+      "^.+\\.(js|jsx)$": "babel-jest"
+    }
   };
   
