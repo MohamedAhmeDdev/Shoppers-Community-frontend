b4f2f5fdff6f069bd75f9f6ca70919893f8dfbee
Mohamed Ahmed <ma07041705@gmail.com>
2024-08-06 21:31:14 +0300

    test

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    presets: [
+      '@babel/preset-env',
+      '@babel/preset-react'
+    ],
+    plugins: [
+      '@babel/plugin-proposal-logical-assignment-operators',
+      // Add other Babel plugins here as needed
+    ]
+  };
+  
