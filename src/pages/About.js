55c64a069e6a2eab577bcdf0b706c2886bdc3b99
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2024-08-07 11:35:12 +0300

    commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,48 +3,7 @@ import React from 'react';
 function About() {
   return (
     <div className="font-roboto w-full bg-white">
-<<<<<<< HEAD
-      
 
-      {/* Main Content */}
-      <main className="container mx-auto py-16 px-4">
-        <section className="flex flex-col md:flex-row items-center mb-12">
-          <div className="md:w-2/3">
-            <h1 className="text-2xl font-bold mb-4">Your number 1 comparison destination for specific products online</h1>
-            <p className="text-gray-600">Since 2024, Shophorizon's mission is to provide seekers with the insights they need to find the best products online based on their industry, size, budget and specific requirements. Every year, we connect millions of people with products and the vendors that meet their needs.</p>
-          </div>
-          <img src="https://pixabay.com/vectors/pugs-dogs-pet-domestic-puppy-7200102/" alt="Comparison Illustration" className="md:w-1/3 mt-6 md:mt-0 md:ml-6" />
-        </section>
-        <section className="flex flex-col md:flex-row items-center mb-12">
-          <div className="md:w-2/3">
-            <h1 className="text-2xl font-bold mb-4">Your number 1 comparison destination for specific products online</h1>
-            <p className="text-gray-600">Since 2024, Shophorizon's mission is to provide seekers with the insights they need to find the best products online based on their industry, size, budget and specific requirements. Every year, we connect millions of people with products and the vendors that meet their needs.</p>
-          </div>
-          <img src="https://pixabay.com/vectors/pugs-dogs-pet-domestic-puppy-7200102/" alt="Comparison Illustration" className="md:w-1/3 mt-6 md:mt-0 md:ml-6" />
-        </section>
-        <section className="flex flex-col md:flex-row items-center mb-12">
-          <div className="md:w-2/3">
-            <h1 className="text-2xl font-bold mb-4">Your number 1 comparison destination for specific products online</h1>
-            <p className="text-gray-600">Since 2024, Shophorizon's mission is to provide seekers with the insights they need to find the best products online based on their industry, size, budget and specific requirements. Every year, we connect millions of people with products and the vendors that meet their needs.</p>
-          </div>
-          <img src="https://pixabay.com/vectors/pugs-dogs-pet-domestic-puppy-7200102/" alt="Comparison Illustration" className="md:w-1/3 mt-6 md:mt-0 md:ml-6" />
-        </section>
-
-        <section className="text-center py-12">
-          <h2 className="text-2xl font-bold mb-6">Customer’s Testimonials</h2>
-          <div className="flex justify-center items-center space-x-4">
-            
-            <div className="bg-white p-6 rounded-full">
-              <img src="https://pixabay.com/photos/cartoon-man-human-person-masculine-871669/" alt="Customer Image" className="rounded-full mx-auto mb-4" />
-              <h3 className="text-xl font-bold">Group 1</h3>
-              <p className="text-gray-600">Customer</p>
-              <p className="mt-4">The best service I have come across and used so far, I would forever recommend to anyone</p>
-            </div>
-
-          </div>
-=======
-
-      
       {/* Main Content */}
       <main className="container mx-auto py-16 px-4">
       <section className="flex flex-col md:flex-row items-center mb-12">
@@ -67,16 +26,21 @@ function About() {
             <p className="text-gray-600">Since 2024, Shophorizon's mission is to provide seekers with the insights they need to find the best products online based on their industry, size, budget and specific requirements. Every year, we connect millions of people with products and the vendors that meet their needs.</p>
           </div>
           <img src="https://cdn.pixabay.com/photo/2018/01/14/23/05/ecommerce-3082813_1280.jpg" alt="Comparison Illustration" className="md:w-1/3 mt-6 md:mt-0 md:ml-6" />
->>>>>>> 70a07410723939bde5c40575c5558271e27ef485
+
         </section>
       </main>
 
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 70a07410723939bde5c40575c5558271e27ef485
+
+
+
+
+
+
     </div>
+
+
   );
 }
 
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,26 +6,7 @@ function Contact() {
 
 
       {/* Main Content */}
-<<<<<<< HEAD
-      <main className="container mx-auto py-16 px-4">
-        <section className="text-center mb-12">
-          <h1 className="text-2xl font-bold mb-4">Contact Us</h1>
-          <img src="https://placehold.co/1000x400" alt="Cityscape" className="mx-auto mb-6" />
-        </section>
-        <section className="grid grid-cols-1 md:grid-cols-3 text-center mb-12">
-          <div className="mb-6 md:mb-0">
-            <i className="fas fa-map-marker-alt fa-3x mb-4 text-black"></i>
-            <h2 className="text-xl font-bold mb-2">Location</h2>
-            <p className="text-gray-600">ShopHorizon<br />Street Lane<br />Avenue 11</p>
-          </div>
-          <div className="mb-6 md:mb-0">
-            <i className="fas fa-phone-alt fa-3x mb-4 text-black"></i>
-            <h2 className="text-xl font-bold mb-2">Telephone</h2>
-            <p className="text-gray-600">+2547012345678</p>
-          </div>
-          <div>
-            <i className="fas fa-envelope fa-3x mb-4 text-black"></i>
-=======
+      
       <main className="w-full py-16 px-4">
       <section className="text-center mb-12 relative">
       <div className="relative w-full mb-6" style={{ height: '450px' }}> 
@@ -51,7 +32,7 @@ function Contact() {
           </div>
           <div className="mb-6 md:mb-0 shadow-lg p-4 rounded">
             <img src="https://cdn-icons-png.flaticon.com/128/159/159052.png" alt="Location Icon" className="mx-auto mb-4 h-12 w-12" />
->>>>>>> 70a07410723939bde5c40575c5558271e27ef485
+
             <h2 className="text-xl font-bold mb-2">Mail</h2>
             <p className="text-gray-600">shophorizon@gmail.com</p>
           </div>
