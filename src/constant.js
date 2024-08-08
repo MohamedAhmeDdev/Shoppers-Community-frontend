ba8c759a2f4906cf64855e91a7185d805aa7867f
Mohamed Ahmed <ma07041705@gmail.com>
2024-08-08 18:03:52 +0300

    server Url

--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,2 @@
-// export const SERVER_URL = "https://shoppers-community-server.onrender.com";
-export const SERVER_URL = "http://127.0.0.1:5555";
+export const SERVER_URL = "https://e-vc24.onrender.com";
+// export const SERVER_URL = "http://127.0.0.1:5555";
