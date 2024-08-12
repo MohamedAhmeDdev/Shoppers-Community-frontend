1649e4ddefcae86a64d3d862f509c5d50bdbb1c6
Mohamed Ahmed <ma07041705@gmail.com>
2024-08-12 11:11:54 +0300

    test

--- src/utils/RouteProtection.js
+++ src/utils/RouteProtection.js
@@ -3,19 +3,17 @@ import { Navigate } from 'react-router-dom';
 
 const getUserFromLocalStorage = () => {
     const user = JSON.parse(localStorage.getItem('user'));
-    const role = localStorage.getItem('role');  // No need to parse role since it's stored as a string
+    const role = localStorage.getItem('role'); 
     return { token: user ? user.token : null, role };
 };
 
 const RouteProtection = ({ element }) => {
     const { token, role } = getUserFromLocalStorage();
 
-    // If the user is authenticated and has the right role
     if (token && role === 'shop owner') {
-        return element;  // Render the protected component
+        return element;
     }
 
-    // If not authenticated, redirect to login page
     return <Navigate to="/login" />;
 };
 
